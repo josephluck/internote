cb0df68cab83036289a42f21313feba5a70b8258
Joseph Luck <joseph.luck@sky.com>
2018-09-15 18:43:05 +0000

    Add server rendered styles

---
+++ ui/pages/_document.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import { colors } from "@internote/ui/styles/theme";
+
+export default class MyDocument extends Document {
+  static async getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="/_next/static/style.css" />
+          <meta name="theme-color" content={colors.cinder} />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, user-scalable=0"
+          />
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
