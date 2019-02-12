9125d11ba13c1e3179b052826e476ac037748c9d
Joseph Luck <joseph.luck@sky.com>
2019-02-12 22:40:37 +0000

    Remove extra padding from note page

--- ui/styles/note.tsx
+++ ui/styles/note.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import dynamic from "next/dynamic";
 import { Store } from "../store";
-import { Box } from "@rebass/grid";
-import { spacing } from "../theming/symbols";
 
 const InternoteEditor = dynamic(
   import("../styles/editor").then(module => module.InternoteEditor),
@@ -13,20 +11,16 @@ const InternoteEditor = dynamic(
 
 export function Note({ store }: { store: Store }) {
   return (
-    <>
-      <Box pb={spacing._3}>
-        <InternoteEditor
-          id={store.state.note.id}
-          initialValue={store.state.note.content}
-          onChange={store.actions.updateNote}
-          onDelete={() =>
-            store.actions.deleteNoteConfirmation({
-              noteId: store.state.note.id
-            })
-          }
-          saving={store.state.loading.updateNote}
-        />
-      </Box>
-    </>
+    <InternoteEditor
+      id={store.state.note.id}
+      initialValue={store.state.note.content}
+      onChange={store.actions.updateNote}
+      onDelete={() =>
+        store.actions.deleteNoteConfirmation({
+          noteId: store.state.note.id
+        })
+      }
+      saving={store.state.loading.updateNote}
+    />
   );
 }
