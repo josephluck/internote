72eab5320b3a67e6b70844d7537dfd2eb99ffe20
Joseph Luck <joseph.luck@sky.com>
2019-05-21 22:50:20 +0100

    Add more safety around dictionary

--- api/domains/dictionary/routes.ts
+++ api/domains/dictionary/routes.ts
@@ -1,7 +1,7 @@
 import * as Router from "koa-router";
 import { Dependencies } from "../../app";
 import { route } from "../router";
-import { Option } from "space-lift";
+import { Option, Some, None } from "space-lift";
 import { UserEntity } from "../entities";
 import { validate, rules } from "../../dependencies/validation";
 import { DictionaryResult } from "./entity";
@@ -63,44 +63,47 @@ function makeController(deps: Dependencies) {
                   .get(`/lemmas/en/${word.toLowerCase()}`)
                   .catch(throwError("Lemmas request failed"));
 
-                const wordId = getWordIdFromLemmasResponse(lemmas.data);
+                return getWordIdFromLemmasResponse(lemmas.data).fold(
+                  () => throwError("Lemmas request failed")(new Error()),
+                  async wordId => {
+                    const entriesResponse = await api
+                      .get(`/entries/en-gb/${wordId}?strictMatch=false`)
+                      .catch(throwError("Entries request failed"));
 
-                const entriesResponse = await api
-                  .get(`/entries/en-gb/${wordId}?strictMatch=false`)
-                  .catch(throwError("Entries request failed"));
-
-                const entries = convertOxfordEntriesResponse(
-                  entriesResponse.data
-                );
-
-                function returnOnlyDictionaryEntries() {
-                  ctx.body = {
-                    results: entries.map(({ thesaurasId, ...rest }) => rest)
-                  };
-                  return ctx.body;
-                }
-
-                try {
-                  const thesaurasResponse = await api
-                    .get(`/thesaurus/en/${wordId}?strictMatch=false`)
-                    .catch(throwError("Thesauras request failed"));
-
-                  if (thesaurasResponse.data) {
-                    const results = mapEntriesToSynonyms(
-                      entries,
-                      thesaurasResponse.data
+                    const entries = convertOxfordEntriesResponse(
+                      entriesResponse.data
                     );
-                    ctx.body = {
-                      results
-                    };
-                    return ctx.body;
-                  } else {
-                    throw new Error("No thesauras entries");
+
+                    function returnOnlyDictionaryEntries() {
+                      ctx.body = {
+                        results: entries.map(({ thesaurasId, ...rest }) => rest)
+                      };
+                      return ctx.body;
+                    }
+
+                    try {
+                      const thesaurasResponse = await api
+                        .get(`/thesaurus/en/${wordId}?strictMatch=false`)
+                        .catch(throwError("Thesauras request failed"));
+
+                      if (thesaurasResponse.data) {
+                        const results = mapEntriesToSynonyms(
+                          entries,
+                          thesaurasResponse.data
+                        );
+                        ctx.body = {
+                          results
+                        };
+                        return ctx.body;
+                      } else {
+                        throw new Error("No thesauras entries");
+                      }
+                    } catch (err) {
+                      console.log("Thesauras was empty", err);
+                      return returnOnlyDictionaryEntries();
+                    }
                   }
-                } catch (err) {
-                  console.log("Thesauras was empty", err);
-                  return returnOnlyDictionaryEntries();
-                }
+                );
               } catch (err) {
                 console.log("Dictionary failed", err);
                 deps.messages.throw(ctx, deps.messages.notFound("Dictionary"));
@@ -131,16 +134,16 @@ function convertOxfordEntriesResponse(
   response: Oxford.EntriesResponse
 ): DictionaryResultWithThesaurasId[] {
   const mappedResult = [] as DictionaryResultWithThesaurasId[];
-  response.results.forEach(({ word, lexicalEntries }) => {
-    lexicalEntries.forEach(({ entries, lexicalCategory }) => {
-      entries.forEach(({ senses }) => {
-        senses.forEach(({ definitions, examples, thesaurusLinks }) => {
+  (response.results || []).forEach(({ word, lexicalEntries }) => {
+    (lexicalEntries || []).forEach(({ entries, lexicalCategory }) => {
+      (entries || []).forEach(({ senses }) => {
+        (senses || []).forEach(({ definitions, examples, thesaurusLinks }) => {
           const thesaurasId =
             thesaurusLinks && thesaurusLinks.length
               ? thesaurusLinks[0].sense_id
               : "";
           const example = examples && examples.length ? examples[0].text : "";
-          definitions.forEach(definition => {
+          (definitions || []).forEach(definition => {
             mappedResult.push({
               word,
               lexicalCategory: lexicalCategory.text,
@@ -163,15 +166,12 @@ function mapEntriesToSynonyms(
 ): DictionaryResult[] {
   return dictionaryResults.map(({ thesaurasId, ...result }) => {
     let synonyms: string[] = [];
-    response.results.forEach(({ lexicalEntries }) => {
-      lexicalEntries.forEach(({ entries }) => {
-        entries.forEach(({ senses }) => {
+    (response.results || []).forEach(({ lexicalEntries }) => {
+      (lexicalEntries || []).forEach(({ entries }) => {
+        (entries || []).forEach(({ senses }) => {
           const sense = senses.find(({ id }) => id === thesaurasId);
           if (sense && sense.synonyms) {
-            synonyms = [
-              ...synonyms,
-              ...sense.synonyms.map(synonym => synonym.text)
-            ];
+            synonyms.concat(sense.synonyms.map(s => s.text));
           }
         });
       });
@@ -180,11 +180,17 @@ function mapEntriesToSynonyms(
   });
 }
 
-function getWordIdFromLemmasResponse(response: Oxford.LemmasResponse): string {
-  const result = response.results[0];
-  const entry = result.lexicalEntries[0];
-  const inflection = entry.inflectionOf[0];
-  return inflection.id;
+function getFirstItemFromArray<A>(arr: A[]): Option<A> {
+  return arr && arr.length > 0 ? Some(arr[0]) : None;
+}
+
+function getWordIdFromLemmasResponse(
+  response: Oxford.LemmasResponse
+): Option<string> {
+  return getFirstItemFromArray(response.results)
+    .flatMap(lemma => getFirstItemFromArray(lemma.lexicalEntries))
+    .flatMap(entry => getFirstItemFromArray(entry.inflectionOf))
+    .map(inflection => inflection.id);
 }
 
 export default routes;
