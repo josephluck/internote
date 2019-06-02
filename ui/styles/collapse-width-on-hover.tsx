b8fd097a6ab8eac2a45d306e56380e0da546c528
Joseph Luck <joseph.luck@sky.com>
2019-06-02 14:05:17 +0100

    Convert collapse width on hover to React Hooks

--- ui/styles/collapse-width-on-hover.tsx
+++ ui/styles/collapse-width-on-hover.tsx
@@ -20,82 +20,63 @@ interface RenderProps {
   renderCollapsedContent: () => React.ReactNode;
 }
 
-interface Props {
+export function CollapseWidthOnHover({
+  className,
+  children,
+  onClick,
+  collapsedContent,
+  forceShow
+}: {
   children: (renderProps: RenderProps) => React.ReactNode;
   collapsedContent: React.ReactNode;
   forceShow?: boolean;
   className?: string;
   onClick?: () => void;
-}
+}) {
+  const [isHovering, setIsHovering] = React.useState(false);
+  const innerWrapRef = React.useRef<HTMLDivElement>();
+  const collapsedContentRef = React.useRef<HTMLDivElement>();
 
-interface State {
-  isHovering: boolean;
-}
-
-export class CollapseWidthOnHover extends React.Component<Props, State> {
-  collapsedContentRef: React.RefObject<HTMLDivElement>;
-  innerWrapRef: React.RefObject<HTMLDivElement>;
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      isHovering: false
-    };
-    this.collapsedContentRef = React.createRef();
-    this.innerWrapRef = React.createRef();
-  }
-
-  componentDidUpdate() {
-    window.requestAnimationFrame(this.handleWidth);
-  }
-
-  handleWidth = () => {
-    const refsExist =
-      this.innerWrapRef.current && this.collapsedContentRef.current;
-    if (refsExist) {
-      if (this.state.isHovering || this.props.forceShow) {
-        const childElm = this.collapsedContentRef.current.firstChild as any;
-        const width = childElm.scrollWidth;
-        this.innerWrapRef.current.style.width = `${width}px`;
-        this.innerWrapRef.current.style.opacity = "1";
-        this.innerWrapRef.current.style.pointerEvents = "auto";
-      } else {
-        this.innerWrapRef.current.style.width = "0px";
-        this.innerWrapRef.current.style.opacity = "0";
-        this.innerWrapRef.current.style.pointerEvents = "none";
+  React.useEffect(() => {
+    function handleWidth() {
+      const refsExist = innerWrapRef.current && collapsedContentRef.current;
+      if (refsExist) {
+        if (isHovering || forceShow) {
+          const childElm = collapsedContentRef.current.firstChild as any;
+          const width = childElm.scrollWidth;
+          innerWrapRef.current.style.width = `${width}px`;
+          innerWrapRef.current.style.opacity = "1";
+          innerWrapRef.current.style.pointerEvents = "auto";
+        } else {
+          innerWrapRef.current.style.width = "0px";
+          innerWrapRef.current.style.opacity = "0";
+          innerWrapRef.current.style.pointerEvents = "none";
+        }
       }
     }
-  };
 
-  onHoverIn = () => {
-    this.setState({ isHovering: true });
-  };
+    window.requestAnimationFrame(handleWidth);
+  }, [innerWrapRef, collapsedContentRef, isHovering, collapsedContent]);
 
-  onHoverOut = () => {
-    this.setState({ isHovering: false });
-  };
+  const onHoverIn = () => setIsHovering(true);
+  const onHoverOut = () => setIsHovering(false);
 
-  renderCollapsedContent = () => {
-    return (
-      <InnerWrap ref={this.innerWrapRef}>
-        <CollapsedContent ref={this.collapsedContentRef}>
-          {this.props.collapsedContent}
-        </CollapsedContent>
-      </InnerWrap>
-    );
-  };
+  const renderCollapsedContent = () => (
+    <InnerWrap ref={innerWrapRef}>
+      <CollapsedContent ref={collapsedContentRef}>
+        {collapsedContent}
+      </CollapsedContent>
+    </InnerWrap>
+  );
 
-  render() {
-    const { className, children, onClick } = this.props;
-    return (
-      <Wrapper
-        className={className}
-        onMouseEnter={this.onHoverIn}
-        onMouseLeave={this.onHoverOut}
-        onClick={onClick}
-      >
-        {children({ renderCollapsedContent: this.renderCollapsedContent })}
-      </Wrapper>
-    );
-  }
+  return (
+    <Wrapper
+      className={className}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+      onClick={onClick}
+    >
+      {children({ renderCollapsedContent })}
+    </Wrapper>
+  );
 }
