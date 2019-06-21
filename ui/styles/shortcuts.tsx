e9c2b8ee7723f8558c719ae549e83d8f0d359a7e
Joseph Luck <joseph.luck@sky.com>
2019-06-21 15:04:09 +0100

    Begin adding shortcuts context

---
+++ ui/styles/shortcuts.tsx
@@ -0,0 +1,138 @@
+import React, { useEffect } from "react";
+import isKeyHotkey from "is-hotkey";
+
+interface Shortcut {
+  /**
+   * A globally unique ID for a shortcut.
+   * Should not clash with any other defined shortcut in the app.
+   */
+  id: string;
+  /**
+   * The keyboard combination used to trigger the callback function
+   */
+  keyCombo: string;
+  /**
+   * The callback to trigger when the keyCombo is pressed by the user.
+   */
+  callback: () => any;
+  /**
+   * When truthy, prevents other shortcuts with the same keyCombo from
+   * being called if this shortcut is higher up in the list of shortcuts.
+   */
+  preventOtherShortcuts?: boolean;
+}
+
+interface Context {
+  /**
+   * The list of currently defined shortcuts.
+   */
+  shortcuts: Shortcut[];
+  /**
+   * Adds a given shortcut to the list of available shortcuts.
+   */
+  addShortcut: (shortcut: Shortcut) => void;
+  /**
+   * Removes a given shortcut from the list of shortcuts.
+   * NB: uses the shortcut's ID property to determine whether
+   * to remove it.
+   */
+  removeShortcut: (shortcut: Shortcut) => void;
+}
+
+const defaultContext: Context = {
+  shortcuts: [],
+  addShortcut() {},
+  removeShortcut() {}
+};
+
+const ShortcutsContext = React.createContext(defaultContext);
+
+export function ShortcutsProvider({ children }: { children: React.ReactNode }) {
+  /**
+   * Adds a given shortcut to the list of available shortcuts.
+   */
+  function addShortcut(shortcut: Shortcut) {
+    setCtx(prevState => {
+      return {
+        ...prevState,
+        shortcuts: [...prevState.shortcuts, shortcut]
+      };
+    });
+  }
+
+  /**
+   * Removes a given shortcut from the list of shortcuts.
+   * NB: uses the shortcut's ID property to determine whether
+   * to remove it.
+   */
+  function removeShortcut(shortcut: Shortcut) {
+    setCtx(prevState => {
+      return {
+        ...prevState,
+        shortcuts: prevState.shortcuts.filter(s => s.id !== shortcut.id)
+      };
+    });
+  }
+
+  const [ctx, setCtx] = React.useState<Context>({
+    shortcuts: [],
+    addShortcut,
+    removeShortcut
+  });
+
+  /**
+   * Binds window events to the shortcut list and
+   * traverses the list when a key combo is pressed.
+   */
+  useEffect(() => {
+    function onKeyDown(event: KeyboardEvent) {
+      let isPrevented = false;
+      ctx.shortcuts.map(shortcut => {
+        if (isKeyHotkey(shortcut.keyCombo, event) && !isPrevented) {
+          if (shortcut.preventOtherShortcuts) {
+            isPrevented = true;
+          }
+          shortcut.callback();
+        }
+      });
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return function() {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [shortcutsHash(ctx.shortcuts)]);
+
+  return (
+    <ShortcutsContext.Provider value={ctx}>
+      {children}
+    </ShortcutsContext.Provider>
+  );
+}
+
+/**
+ * Adds a shortcut to the list of available shortcuts in the app
+ * when mounted.
+ *
+ * When unmounted, removes the shortcut from the list of available
+ * shortcuts.
+ */
+function Shortcut(shortcut: Shortcut) {
+  const { addShortcut, removeShortcut } = React.useContext(ShortcutsContext);
+  useEffect(() => {
+    addShortcut(shortcut);
+    return function() {
+      removeShortcut(shortcut);
+    };
+  }, [shortcut.id, shortcut.keyCombo]);
+
+  return null;
+}
+
+/**
+ * Computes a unique hash of the list of shortcuts for effective
+ * diffing two lists of shortcuts for strict equality
+ */
+function shortcutsHash(shortcuts: Shortcut[]): string {
+  return shortcuts.reduce((prev, shortcut) => `${prev}-${shortcut.id}`, "");
+}
