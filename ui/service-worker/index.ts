7af1c0cb4a75296f66cae754bc99fabcae1e89e2
Joseph Luck <joseph.luck@sky.com>
2019-08-12 09:11:14 +0100

    Fix response of SW

--- ui/service-worker/index.ts
+++ ui/service-worker/index.ts
@@ -1,20 +1,25 @@
-const sleep = (duration: number = 5000) =>
-  new Promise(resolve => setTimeout(resolve, duration));
+declare const self: ServiceWorkerGlobalScope;
 
-self.addEventListener("sync", async (event: any) => {
-  await event.waitUntil(sleep(5000));
-  console.log("Done");
-  event.respondWith("Done!");
-});
+// const sleep = (duration: number = 5000) =>
+//   new Promise(resolve => setTimeout(resolve, duration));
+
+const log = (msg: string, ...args: any[]) =>
+  console.log(`[SW] ${msg}`, ...args);
 
 self.addEventListener("fetch", async (event: any) => {
-  console.log("Caught request for", event.request.url);
-  await sleep(1000);
-  console.log("Finished sleeping for 1s");
-  const response = await fetch(event.request);
-  console.log("Response", { response });
-  event.respondWith(response);
+  const handler = async () => {
+    log("Handling fetch request", { event });
+    // await sleep(5000);
+    log("Slept for 5s");
+    const response = await fetch(event.request);
+    log("Response", { response });
+    return response;
+  };
+
+  event.waitUntil(handler());
 });
+
+self.addEventListener("activate", () => self.clients.claim());
 
 // TODO: All files must be modules when the '--isolatedModules' flag is provided.
 export const foo = null;
