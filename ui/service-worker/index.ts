dcd8f5bd62256cbf0be93cea542f5ac42575f471
Joseph Luck <joseph.luck@sky.com>
2019-08-12 15:01:24 +0100

    Switch up API for service worker router

--- ui/service-worker/index.ts
+++ ui/service-worker/index.ts
@@ -9,20 +9,18 @@ const log = (msg: string, ...args: any[]) =>
   console.log(`[SW] ${msg}`, ...args);
 
 self.addEventListener("fetch", async (event: any) => {
-  const handler = router([
-    {
-      path: "notes/:id",
-      handler: async event => {
-        log("Handling fetch request", { event });
-        // await sleep(5000);
-        log("Slept for 5s");
-        const response = await fetch(event.request);
-        log("Response", { response });
-        return response;
-      }
-    }
-  ]);
-  event.waitUntil(handler(event));
+  const rtr = router();
+
+  rtr.add("notes/:id", async event => {
+    log("Handling fetch request", { event });
+    // await sleep(5000);
+    log("Slept for 5s");
+    const response = await fetch(event.request);
+    log("Response", { response });
+    return response;
+  });
+
+  event.waitUntil(rtr.handle(event));
 });
 
 self.addEventListener("activate", () => self.clients.claim());
@@ -30,13 +28,20 @@ self.addEventListener("activate", () => self.clients.claim());
 // TODO: All files must be modules when the '--isolatedModules' flag is provided.
 export const foo = null;
 
+type Handler = (event: any) => Promise<any>;
+
 interface Route {
   path: string;
-  handler: (event: any) => Promise<any>;
+  handler: Handler;
 }
 
-const router = (routes: Route[]) => {
-  return (event: any) => {
+const router = () => {
+  let routes: Route[] = [];
+
+  const add = (path: string, handler: Handler) =>
+    routes.push({ path, handler });
+
+  const handle = (event: any) => {
     const matches = routes.map(route => {
       const pattern = new UrlPattern(route.path);
       return pattern.match(event.request.url);
@@ -48,4 +53,6 @@ const router = (routes: Route[]) => {
       event.waitUntil(fetch(event.request));
     }
   };
+
+  return { add, handle };
 };
