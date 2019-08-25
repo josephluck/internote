48b5e649f0f04d5895aef7445ddc914a999d1336
Joseph Luck <joseph.luck@sky.com>
2019-08-25 18:37:08 +0100

    Add validation to create snippet

--- services/snippets/create.ts
+++ services/snippets/create.ts
@@ -1,14 +1,22 @@
 import uuid from "uuid";
 import middy from "middy";
 import { jsonBodyParser, cors } from "middy/middlewares";
-import { encodeResponse, jsonErrorHandler } from "@internote/lib/middlewares";
+import {
+  encodeResponse,
+  jsonErrorHandler,
+  validateRequestBody
+} from "@internote/lib/middlewares";
 import { success } from "@internote/lib/responses";
 import { getUserIdentityId } from "@internote/lib/user";
 import { createSnippet } from "./db/queries";
 import { CreateHandler } from "@internote/lib/types";
 import { CreateSnippetDTO } from "./types";
+import { required, isString } from "@internote/lib/validator";
 
-// TODO: validation
+const validator = validateRequestBody<CreateSnippetDTO>({
+  title: [required, isString],
+  content: [required]
+});
 
 const create: CreateHandler<CreateSnippetDTO> = async (
   event,
@@ -23,6 +31,7 @@ const create: CreateHandler<CreateSnippetDTO> = async (
 
 export const handler = middy(create)
   .use(jsonBodyParser())
+  .use(validator)
   .use(encodeResponse())
   .use(jsonErrorHandler())
   .use(cors());
