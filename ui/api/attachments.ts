ccff8b991250c800bcf1ed6617f9a0943abbce33
Joseph Luck <joseph.luck@sky.com>
2019-08-29 23:08:14 +0100

    Move to aws-sdk for S3 upload

--- ui/api/attachments.ts
+++ ui/api/attachments.ts
@@ -1,8 +1,7 @@
-import { Err, Ok } from "space-lift";
 import "isomorphic-fetch";
-import aws4 from "aws4";
 import { Session } from "../auth/storage";
 import { decodeIdToken } from "../auth/decode";
+import { S3 } from "aws-sdk";
 
 export type MakeSignedRequest = (options: AwsSignedRequest) => any;
 
@@ -15,66 +14,35 @@ export interface AwsSignedRequest {
 
 export function makeAttachmentsApi({
   region,
-  host
+  bucketName
 }: {
   /** AWS region for services */
   region: string;
-  /** Host path for S3 bucket (see env.reference ATTACHMENTS_S3_HOST) */
-  host: string;
+  /** Name of the bucket */
+  bucketName: string;
 }) {
-  const makeSignedRequest: MakeSignedRequest = async ({
-    path,
-    session,
-    method,
-    body
-  }) => {
-    const request = aws4.sign(
-      {
-        host,
-        path,
-        url: `${host}${path}`,
-        method,
-        region,
-        service: "s3",
-        data: body,
-        body
-      },
-      {
-        accessKeyId: session.accessKeyId,
-        secretAccessKey: session.secretKey,
-        sessionToken: session.sessionToken
-      }
-    );
-    const response = await fetch(request.url, {
-      method,
-      headers: request.headers,
-      body
-    });
-    if (!response.ok) {
-      try {
-        const json = await response.json();
-        return Err(json);
-      } catch (err) {
-        return Err("Response error was not JSON");
-      }
-    }
-    try {
-      const json = await response.json();
-      return Ok(json);
-    } catch (err) {
-      return Err("Response error was not JSON");
-    }
-  };
-
-  async function uploadFile(session: Session, file: File): Promise<void> {
+  async function uploadFile(session: Session, file: File): Promise<any> {
     const { sub } = decodeIdToken(session.idToken);
-    const response = await makeSignedRequest({
-      session,
-      body: file,
-      path: `/private/${sub}/${Date.now()}`,
-      method: "POST"
+
+    // TODO: https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-browser.html
+    const s3Instance = new S3({ region });
+    const request = s3Instance.upload({
+      Key: `private/${sub}/${Date.now()}/${file.name}`,
+      Bucket: bucketName,
+      Body: file,
+      ContentType: file.type
     });
-    console.log(response);
+
+    return new Promise((resolve, reject) => {
+      request.send((err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log({ data });
+          resolve(data);
+        }
+      });
+    });
   }
   return {
     uploadFile
