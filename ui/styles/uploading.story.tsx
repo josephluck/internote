6851dfac4ee337e9240e882ff4c4a0f1df65613a
Joseph Luck <joseph.luck@sky.com>
2019-09-04 23:18:26 +0100

    Add uploading story

---
+++ ui/styles/uploading.story.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StoriesOf } from "../types";
+import { Uploading } from "./uploading";
+import { UnknownFile } from "./unknown-file";
+
+export default function(s: StoriesOf) {
+  s("Uploading", module)
+    .add("No progress", () => <Uploading name="file.md" progress={0} />)
+    .add("In progress", () => <Uploading name="file.md" progress={30} />)
+    .add("Uploaded", () => <Uploading name="file.md" progress={100} />)
+    .add("With file above", () => (
+      <div>
+        <UnknownFile
+          name="file.md"
+          extension="md"
+          onDownloadFile={console.log}
+        />
+        <br />
+        <br />
+        <Uploading name="file.md" progress={30} />
+      </div>
+    ));
+}
