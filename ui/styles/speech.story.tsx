2719331ee674894be6c59d84e581d8b8c8b6b60f
Joseph Luck <joseph.luck@sky.com>
2019-09-05 22:27:12 +0100

    Add more stories

---
+++ ui/styles/speech.story.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { StoriesOf } from "../types";
+import { Speech } from "./speech";
+
+export default function(s: StoriesOf) {
+  s("Speech", module)
+    .add("With audio file", () => (
+      <Speech
+        src="https://sample-videos.com/audio/mp3/crowd-cheering.mp3"
+        isLoading={false}
+        onRequest={console.log}
+        onDiscard={console.log}
+        onFinished={console.log}
+      />
+    ))
+    .add("Without audio file", () => (
+      <Speech
+        src=""
+        isLoading={false}
+        onRequest={console.log}
+        onDiscard={console.log}
+        onFinished={console.log}
+      />
+    ))
+    .add("Loading", () => (
+      <Speech
+        src=""
+        isLoading
+        onRequest={console.log}
+        onDiscard={console.log}
+        onFinished={console.log}
+      />
+    ));
+}
---
+++ ui/styles/tag.story.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StoriesOf } from "../types";
+import { Tag, NewTag } from "./tag";
+
+export default function(s: StoriesOf) {
+  s("Tag", module)
+    .add("Not focused", () => <Tag isFocused={false}>#internote</Tag>)
+    .add("Focused", () => <Tag isFocused>#internote</Tag>)
+    .add("Large", () => (
+      <Tag isFocused={false} large>
+        #internote
+      </Tag>
+    ))
+    .add("New tag", () => <NewTag isFocused={false}>Create #design</NewTag>)
+    .add("New tag large", () => (
+      <NewTag isFocused={false} large>
+        Create #design
+      </NewTag>
+    ));
+}
