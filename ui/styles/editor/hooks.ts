bf12ea20a0516b61cb9df0a00533689503fddb36
Joseph Luck <joseph.luck@sky.com>
2020-06-10 21:11:38 +0100

    Add markdown shortcuts

--- ui/styles/editor/hooks.ts
+++ ui/styles/editor/hooks.ts
@@ -3,11 +3,12 @@ import { withHistory } from "slate-history";
 import { withReact, useSlate } from "slate-react";
 import { createEditor } from "slate";
 import { InternoteSlateEditor } from "./types";
+import { withShortcuts } from "./shortcuts";
 
 export const useCreateInternoteEditor = () =>
-  useMemo(() => withHistory(withReact(createEditor())), []);
+  useMemo(() => withHistory(withShortcuts(withReact(createEditor()))), []);
 
 export const useInternoteEditor = (): InternoteSlateEditor => {
   const editor = useSlate();
-  return editor as InternoteSlateEditor;
+  return (editor as any) as InternoteSlateEditor;
 };
