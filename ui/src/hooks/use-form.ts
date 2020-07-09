43cb48782234d89bb1514bac2ebf58c683f659d2
Joseph Luck <joseph.luck@sky.com>
2020-07-09 21:35:07 +0100

    Add validation and better state management for authentication forms

---
+++ ui/src/hooks/use-form.ts
@@ -0,0 +1,190 @@
+import { FieldConstraintsMap, makeValidator } from "@josephluck/valley/lib/fp";
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/pipeable";
+import { useState } from "react";
+
+import { InputProps } from "../styles/input";
+
+type Fields = Record<string, any>;
+
+type FilterType<O, T> = { [K in keyof O]: O[K] extends T ? K : never };
+
+type FilterTypeForKeys<O, T> = FilterType<O, T>[keyof O];
+
+export function useForm<Fs extends Fields>(
+  initialValues: Fs,
+  constraints: FieldConstraintsMap<Fs>
+) {
+  type StringKeys = FilterTypeForKeys<Fs, string>;
+
+  type NumberKeys = FilterTypeForKeys<Fs, number>;
+
+  const doValidate = makeValidator<Fs>(constraints);
+
+  const [values, setValues] = useState(initialValues);
+
+  const [touched, setTouched] = useState(() =>
+    getInitialTouched(initialValues)
+  );
+
+  const [errors, setErrors] = useState<Record<keyof Fs, string>>(() =>
+    pipe(
+      doValidate(initialValues),
+      E.swap,
+      E.getOrElse(() => getInitialErrors(initialValues))
+    )
+  );
+
+  const validate = (
+    currentValues: Fs = values
+  ): ReturnType<typeof doValidate> =>
+    pipe(
+      doValidate(currentValues),
+      E.mapLeft((e) => {
+        setErrors(e);
+        return e;
+      }),
+      E.map((v) => {
+        setErrors(getInitialErrors(initialValues));
+        return v;
+      })
+    );
+
+  const setAllTouched = () => {
+    setTouched(
+      Object.keys(initialValues).reduce(
+        (acc, key) => ({ ...acc, [key]: true }),
+        initialValues
+      )
+    );
+  };
+
+  const setValue = <Fk extends keyof Fs>(fieldKey: Fk, fieldValue: Fs[Fk]) => {
+    const newValues: Fs = { ...values, [fieldKey]: fieldValue };
+    setValues(newValues);
+    validate(newValues);
+  };
+
+  const setValuesAndValidate = (vals: Partial<Fs>) => {
+    const newValues: Fs = { ...values, ...vals };
+    setValues(newValues);
+    validate(newValues);
+  };
+
+  const submit = () => {
+    setAllTouched();
+    return validate();
+  };
+
+  const reset = () => {
+    setValues(initialValues);
+    setTouched(getInitialTouched(initialValues));
+    setErrors(getInitialErrors(initialValues));
+  };
+
+  const registerBlur = <Fk extends keyof Fs>(fieldKey: Fk) => () => {
+    setTouched((current) => ({ ...current, [fieldKey]: true }));
+  };
+
+  const registerOnChangeText = <Fk extends keyof Fs>(fieldKey: Fk) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setValue(fieldKey, event.target.value as Fs[Fk]);
+  };
+
+  const registerOnChangeNumber = <Fk extends keyof Fs>(fieldKey: Fk) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setValue(fieldKey, numberify(event.target.value) as Fs[Fk]);
+  };
+
+  const registerTextInput = <Fk extends StringKeys>(
+    fieldKey: Fk
+  ): Partial<InputProps> => ({
+    value: values[fieldKey],
+    error: errors[fieldKey],
+    touched: touched[fieldKey],
+    onBlur: registerBlur(fieldKey),
+    onChange: registerOnChangeText(fieldKey),
+  });
+
+  const registerNumberInput = <Fk extends NumberKeys>(
+    fieldKey: Fk
+  ): Partial<InputProps> => ({
+    value: values[fieldKey] ? values[fieldKey].toString() : values[fieldKey],
+    error: errors[fieldKey],
+    touched: touched[fieldKey],
+    onBlur: registerBlur(fieldKey),
+    onChange: registerOnChangeNumber(fieldKey),
+  });
+
+  return {
+    values,
+    touched,
+    errors,
+    validate,
+    setValue,
+    setValues: setValuesAndValidate,
+    reset,
+    submit,
+    registerTextInput,
+    registerNumberInput,
+  };
+}
+
+const getInitialTouched = <Fs extends Record<string, any>>(
+  fields: Fs
+): Record<keyof Fs, boolean> =>
+  Object.keys(fields).reduce(
+    (acc, fieldKey) => ({ ...acc, [fieldKey]: false }),
+    fields
+  );
+
+const getInitialErrors = <Fs extends Record<string, any>>(
+  fields: Fs
+): Record<keyof Fs, string> =>
+  Object.keys(fields).reduce(
+    (acc, fieldKey) => ({ ...acc, [fieldKey]: "" }),
+    fields
+  );
+
+const isRequired = <T>(value: T): E.Either<string, T> =>
+  Boolean(value) ? E.right(value) : E.left("Required");
+
+const isOfType = (type: string) => <T>(value: T): E.Either<string, T> =>
+  typeof value === type ? E.right(value) : E.left(`Expected a ${type}`);
+
+const isString = isOfType("string");
+
+const isNumber = isOfType("number");
+
+const isEqualTo = <T>(expected: T) => <V extends T>(
+  value: V
+): E.Either<string, V> =>
+  value === expected
+    ? E.right(value)
+    : E.left(`Expected ${value} to equal ${expected}`);
+
+const isLengthAtLeast = (length: number) => (
+  value: string
+): E.Either<string, string> =>
+  value.length >= length
+    ? E.right(value)
+    : E.left(`Must be at least ${length} characters long`);
+
+export const constraints = {
+  isRequired,
+  isOfType,
+  isString,
+  isNumber,
+  isEqualTo,
+  isLengthAtLeast,
+};
+
+const numberify = (val: string): number | undefined => {
+  const numbersOnly = val.replace(/\D/g, "");
+  if (Number.isNaN(parseInt(numbersOnly))) {
+    return undefined;
+  }
+  return parseInt(numbersOnly);
+};
--- ui/src/pages/authenticate.tsx
+++ ui/src/pages/authenticate.tsx
@@ -1,8 +1,11 @@
 import { RouteComponentProps, navigate } from "@reach/router";
 import { Box, Flex } from "@rebass/grid";
-import React, { useCallback } from "react";
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/pipeable";
+import React from "react";
 import styled from "styled-components";
 
+import { constraints, useForm } from "../hooks/use-form";
 import { signUp, verify } from "../store/auth/auth";
 import { useLoadingAction, useStately } from "../store/store";
 import { Button } from "../styles/button";
@@ -26,78 +29,93 @@ const Instructions = styled.p`
 
 export const Authenticate: React.FunctionComponent<RouteComponentProps> = () => {
   const needsVerify = useStately((state) => state.auth.needsVerify);
+  return (
+    <Modal open showCloseIcon={false} onClose={() => null}>
+      <CenteredLogo>
+        <Logo large>Internote</Logo>
+      </CenteredLogo>
+      {needsVerify ? <VerifyForm /> : <EmailForm />}
+    </Modal>
+  );
+};
 
+const EmailForm: React.FunctionComponent = () => {
   const handleAuthenticate = useLoadingAction(signUp);
-  const handleVerify = useLoadingAction(verify);
 
-  const handleSubmit = useCallback(
-    async (code: string) => {
-      await handleVerify.exec({ code });
-      navigate("/");
+  const { submit, registerTextInput } = useForm<{ email: string }>(
+    {
+      email: "",
     },
-    [handleVerify]
+    {
+      email: [constraints.isRequired],
+    }
   );
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    pipe(submit(), E.map(handleAuthenticate.exec));
+  };
+
   return (
-    <>
-      <Modal open showCloseIcon={false} onClose={() => null}>
-        <CenteredLogo>
-          <Logo large>Internote</Logo>
-        </CenteredLogo>
-        {needsVerify ? (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleSubmit((document.getElementById("verify") as any).value);
-            }}
-          >
-            <Box mb={spacing._1_5}>
-              <Instructions>
-                A verification code has been sent to you. Please enter it below
-                to sign in:
-              </Instructions>
-            </Box>
-            <Flex mb={spacing._1} flexDirection="column">
-              <InputLabel>Verification code</InputLabel>
-              <Input autoFocus id="verify" />
-            </Flex>
-            <Box mb={spacing._1}>
-              <Button
-                type="submit"
-                primary
-                fullWidth
-                loading={handleAuthenticate.loading}
-              >
-                Sign in
-              </Button>
-            </Box>
-          </form>
-        ) : (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleAuthenticate.exec({
-                email: (document.getElementById("email") as any).value,
-              });
-            }}
-          >
-            <Flex mb={spacing._1} flexDirection="column">
-              <InputLabel>Email</InputLabel>
-              <Input autoFocus type="email" id="email" />
-            </Flex>
-            <Box mb={spacing._1}>
-              <Button
-                type="submit"
-                primary
-                fullWidth
-                loading={handleVerify.loading}
-              >
-                Enter
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Modal>
-    </>
+    <form onSubmit={handleSubmit}>
+      <Flex mb={spacing._1} flexDirection="column">
+        <InputLabel>Email</InputLabel>
+        <Input autoFocus type="email" {...registerTextInput("email")} />
+      </Flex>
+      <Box mb={spacing._1}>
+        <Button
+          type="submit"
+          primary
+          fullWidth
+          loading={handleAuthenticate.loading}
+        >
+          Enter
+        </Button>
+      </Box>
+    </form>
+  );
+};
+
+const VerifyForm: React.FunctionComponent = () => {
+  const handleVerify = useLoadingAction(verify);
+
+  const { submit, registerTextInput } = useForm<{ code: string }>(
+    {
+      code: "",
+    },
+    {
+      code: [constraints.isRequired],
+    }
+  );
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    pipe(
+      submit(),
+      E.map(async (fields) => {
+        await handleVerify.exec(fields);
+        navigate("/");
+      })
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box mb={spacing._1_5}>
+        <Instructions>
+          A verification code has been sent to you. Please enter it below to
+          sign in:
+        </Instructions>
+      </Box>
+      <Flex mb={spacing._1} flexDirection="column">
+        <InputLabel>Verification code</InputLabel>
+        <Input autoFocus {...registerTextInput("code")} />
+      </Flex>
+      <Box mb={spacing._1}>
+        <Button type="submit" primary fullWidth loading={handleVerify.loading}>
+          Sign in
+        </Button>
+      </Box>
+    </form>
   );
 };
